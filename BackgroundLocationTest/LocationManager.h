6a0ddf8348fa8a8418924a5d25c71a9a4c82f4d2
Vibhanshu <vibhanshu.jain@cubastion.com>
2020-04-27 14:20:46 +0530

    first commit

---
+++ BackgroundLocationTest/LocationManager.h
@@ -0,0 +1,17 @@
+//
+//  LocationManager.h
+//  BackgroundLocationTest
+//
+//  Created by Vibhanshu Jain on 21/04/20.
+//  Copyright © 2020 Vibhanshu Jain. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LocationManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BackgroundLocationTest/LocationManager.m
@@ -0,0 +1,13 @@
+//
+//  LocationManager.m
+//  BackgroundLocationTest
+//
+//  Created by Vibhanshu Jain on 21/04/20.
+//  Copyright © 2020 Vibhanshu Jain. All rights reserved.
+//
+
+#import "LocationManager.h"
+
+@implementation LocationManager
+
+@end
